3a567d8d258231ac1114022271fc0cf2f453897a
lagaa <s288032@studenti.polito.it>
2025-10-28 15:02:29 +0100

    Debug blob length

--- keylime/tpm/tpm2_objects.py
+++ keylime/tpm/tpm2_objects.py
@@ -267,6 +267,7 @@ def _curve_from_curve_id(cid: int) -> EllipticCurve:
 
 def _extract_tpm2b(vals: bytes) -> Tuple[bytes, bytes]:
     (length,) = struct.unpack(">H", vals[0:2])
+    print('The length is: ' + length + '\n')
     # Ignore the length itself when returning
     vals = vals[2:]
     # Return first the currect buffer, and then the rest
@@ -276,14 +277,19 @@ def _extract_tpm2b(vals: bytes) -> Tuple[bytes, bytes]:
 def pubkey_parms_from_tpm2b_public(
     public: bytes,
 ) -> Tuple[pubkey_type, int]:
+
+    print('The very first public material is:\n\n' + str(public) + '\n\nAnd its length is: ' + str(len(str(public))))
     (public, rest) = _extract_tpm2b(public)
     if len(rest) != 0:
         raise ValueError("More in tpm2b_public than tpmt_public")
     # Extract type, nameAlg, and [objectAttributes] (we don't care about the
     #  latter)
-    (alg_type, name_alg, _) = struct.unpack(">HHI", public[0:8])
+    print('The public material after:\n\n' + str(public))
+    (alg_type, name_alg, _) = struct.unpack(">HHI", public[0:8])            # > ---> means "Big Endian". H ---> 2 byte. I ---> 4 byte.
+    
+    # alg_type --> 2 byte. name_alg --> 2 byte.
 
-    print('The public material is: ' + str(alg_type) + ' ' +  str(name_alg) + '\n')
+    print('The algorithm for public/private key is: ' + str(alg_type) + '\nThe hashing algorithm is: ' +  str(name_alg) + '\n')
     # Ignore the authPolicy
     (_, sym_parms) = _extract_tpm2b(public[8:])
     # Ignore the non-asym-alg parameters
@@ -297,6 +303,9 @@ def pubkey_parms_from_tpm2b_public(
     if scheme_alg != TPM2_ALG_NULL:
         to_skip = to_skip + 2
     asym_parms = sym_parms[to_skip:]
+
+    print('Asym parms: ' + str(asym_parms) + '\nAnd its lenght is: ' + str(len(str(asym_parms))))
+    print('\nsym_alg: ' + str(sym_alg) + '\nBits of the Asymmetric Algorithm: ' + str(scheme_alg) + '\n')
 
     # Handle fields
     if alg_type == TPM_ALG_RSA:
@@ -334,7 +343,6 @@ def pubkey_parms_from_tpm2b_public(
 
     if alg_type == TPM_ALG_MLDSA:
         print('MLDSA  case\n')
-        print('sym parms: ' + str(asym_parms) + '\nsym_alg: ' + str(sym_alg) + '\nscheme_alg: ' + str(scheme_alg) + '\n')
 
     raise ValueError(f"Invalid tpm2b_public type: {alg_type}")
 
